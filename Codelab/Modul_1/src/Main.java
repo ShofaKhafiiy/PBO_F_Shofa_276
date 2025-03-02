4a03406c37f05d524840bb9ef087ef29aa01c886
SSS <shofakhafidin@gmail.com>
2025-03-02 19:47:35 +0700

    menambahkan tugas codelab modul 1

--- Codelab/Modul_1/src/Main.java
+++ Codelab/Modul_1/src/Main.java
@@ -1,15 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+import  java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);//system in menunjukan bahwa input akan diterima keyboard
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //untuk menginput nama
+        System.out.print("Masukan nama: ");
+        String nama= scanner.nextLine(); //mengambil inputan nama dari pengguna
+
+        //input jenis kelamin
+        System.out.print("Masukan jenis kelamin(P/L): ");
+        char JenisKelamin = scanner.next().charAt(0);
+        scanner.nextLine(); //membersihkan newline dari inputan sebelumnya
+
+        // input tahun lahir(harus berupa angka)
+        System.out.print("Masukan tahun lahir: ");
+        int Tahun_lahir = scanner.nextInt();
+        scanner.close(); // menutup object scan unutk mencegah kebocoran memori
+        //Menghitung Umur berdasarkan tahun lahir
+        int tahunSekarang = LocalDate.now().getYear(); //Mengambil tahun ini dengan menggunakan API localDate
+        int Umur = tahunSekarang - Tahun_lahir; //mengurangi tahun saat ini dengan inputan tahun lahir kita untuk mendapatkan umur kita
+
+        //Menentukan jenis kelamin berdasarkan input
+        //Menggunakan validasi
+        String jenisKelaminStr = "";
+
+        if (JenisKelamin == 'L'|| JenisKelamin == 'l'){
+            jenisKelaminStr = "laki-laki";
+        } else if (JenisKelamin == 'P' || JenisKelamin == 'p') {
+            jenisKelaminStr = "Perempuan";
+
         }
+        else {
+            jenisKelaminStr = "Tidak Diketahui";
+        }
+
+        //Menampilkan output data diri
+        System.out.println("\nData Diri:");
+        System.out.println("Nama            : "+nama);
+        System.out.println("Jenis Kelamin   : "+ jenisKelaminStr);
+        System.out.println("Umur            : "+Umur+"tahun");
     }
 }
